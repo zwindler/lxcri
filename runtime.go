65dcafdc625ecdf138bd1b5ac8fef3cc05806a20
Ruben Jenster <r.jenster@drachenfels.de>
2021-05-27 11:37:08 +0200

    Fix hasCapability check.
    Signed-off-by: Ruben Jenster <r.jenster@drachenfels.de>

--- runtime.go
+++ runtime.go
@@ -104,7 +104,7 @@ func (rt *Runtime) hasCapability(s string) bool {
 		rt.Log.Warn().Msgf("undefined capability %q", s)
 		return false
 	}
-	return rt.caps.Get(capability.EFFECTIVE, c)
+	return rt.caps.Get(capability.CAPS, c)
 }
 
 func (rt *Runtime) isPrivileged() bool {
